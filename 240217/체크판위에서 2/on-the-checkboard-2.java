7649ab42109c32af051947687f25a9d13c2f884c
Eunnyeong Choi <41516670+doseee@users.noreply.github.com>
2024-02-17 01:29:00 +0900

    Title: 체크판위에서 2, Time: 149ms, Memory: 10MB, Status: Passed - CodeTree

--- 240217/체크판위에서 2/on-the-checkboard-2.java
+++ 240217/체크판위에서 2/on-the-checkboard-2.java
@@ -1,43 +1,35 @@
-import java.util.*;
-import java.io.*;
+import java.util.Scanner;
 
 public class Main {
+    public static final int MAX_N = 15;
 
-  static char[][] a;
-  static int n, c, cnt;
-
-  public static void main(String[] args) throws IOException{
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-    StringTokenizer st = new StringTokenizer(br.readLine());
-    n = Integer.parseInt(st.nextToken());
-    c = Integer.parseInt(st.nextToken());
-
-    a = new char[n][c];
-    for (int i = 0; i < n; i++) {
-      st = new StringTokenizer(br.readLine());
-      for (int j = 0; j < c; j++)
-        a[i][j] = st.nextToken().charAt(0);
-    }
-
-    cnt = 0;
-    dfs(0, 0, 1);
-
-    System.out.print(cnt);
-  }
-
-  public static void dfs(int x, int y, int d) {
-    if (d == 4 && x == n - 1 && y == c - 1) {
-      cnt++;
-      return;
-    }
-
-    char ch = a[x][y];
-    for (int i = x + 1; i < n; i++)
-      for (int j = y + 1; j < c; j++)
-        if (a[i][j] != ch)
-          dfs(i, j, d + 1);
+    public static int n, m;
+    public static char[][] grid = new char[MAX_N][MAX_N];
     
-  }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
+        // 입력
+        n = sc.nextInt();
+        m = sc.nextInt();
+        for(int i = 0; i < n; i++)
+            for(int j = 0; j < m; j++) {
+                grid[i][j] = sc.next().charAt(0);
+            }
+
+        // 이동 시에 행과 열이 전부 증가하도록
+        // 모든 쌍을 다 잡아봅니다.
+        int cnt = 0;
+        for(int i = 1; i < n; i++)
+            for(int j = 1; j < m; j++)
+                for(int k = i + 1; k < n - 1; k++)
+                    for(int l = j + 1; l < m - 1; l++)
+                        // 그 중 색깔이 전부 달라지는 경우에만 개수를 세줍니다.
+                        if(grid[0][0] != grid[i][j] && 
+                           grid[i][j] != grid[k][l] &&
+                           grid[k][l] != grid[n - 1][m - 1])
+                            cnt++;
+        
+        System.out.println(cnt);
+    }
 }
