1f8e105af1aabb23c8fd2d99f810df68c6880412
Eunnyeong Choi <41516670+doseee@users.noreply.github.com>
2024-02-17 18:05:38 +0900

    Title: 마라톤 중간에 택시타기 2, Time: 112ms, Memory: 8MB, Status: Passed - CodeTree

--- 240217/마라톤 중간에 택시타기 2/taking-a-taxi-in-the-middle-of-the-marathon-2.java
+++ 240217/마라톤 중간에 택시타기 2/taking-a-taxi-in-the-middle-of-the-marathon-2.java
@@ -1,5 +1,36 @@
+import java.util.*;
+import java.io.*;
+import java.awt.Point;
+
 public class Main {
-    public static void main(String[] args) {
-        // 여기에 코드를 작성해주세요.
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    int n = Integer.parseInt(br.readLine());
+    Point[] a = new Point[n];
+    for (int i = 0; i < n; i++) {
+      StringTokenizer st = new StringTokenizer(br.readLine());
+      int x = Integer.parseInt(st.nextToken());
+      int y = Integer.parseInt(st.nextToken());
+      a[i] = new Point(x, y);
     }
+
+    int idx = 1, ans = Integer.MAX_VALUE;
+    for (int i = 0; i < n - 2; i++) {
+      int sum = 0;
+      for (int j = 0; j < n - 1; j++) {
+        if (j + 1 == idx) {
+          sum += Math.abs(a[j].x - a[j + 2].x) + Math.abs(a[j].y - a[j + 2].y);
+          j++;
+        }
+        else
+          sum += Math.abs(a[j].x - a[j + 1].x) + Math.abs(a[j].y - a[j + 1].y);
+      }
+      ans = Math.min(sum, ans);
+      idx++;
+    }
+
+    System.out.println(ans);
+  }
 }
