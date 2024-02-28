f785c430a728f08a0c51eae53f474283cc891f4c
Eunnyeong Choi <41516670+doseee@users.noreply.github.com>
2024-02-28 23:21:43 +0900

    Title: 특정 수와 근접한 합, Time: 3073ms, Memory: 9MB, Status: Time Limit Exceed - CodeTree

---
+++ 240228/특정 수와 근접한 합/sum-close-to-particular-number.java
@@ -0,0 +1,43 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+  static int n, s, ans;
+  static int[] a, select;
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    n = Integer.parseInt(st.nextToken());
+    s = Integer.parseInt(st.nextToken());
+    
+    a = new int[n];
+    st = new StringTokenizer(br.readLine());
+    for (int i = 0; i < n; i++)
+      a[i] = Integer.parseInt(st.nextToken());
+    
+    select = new int[n - 2];
+    ans = Integer.MAX_VALUE;
+    comb(0, 0);
+    
+    System.out.print(ans);
+  }
+  
+  public static void comb(int cnt, int idx) {
+    if (cnt == n - 2) {
+      int sum = 0;
+      for (int i = 0; i < cnt; i++)
+        sum += select[i];
+      
+      ans = Math.min(ans, Math.abs(sum - s));
+      return;
+    }
+    
+    for (int i = idx; i < n; i++) {
+      select[cnt] = a[i];
+      comb(cnt + 1, i + 1);
+    }
+  }
+}
