4af8921a9fdbfe07f2609860e845036a81786774
Eunnyeong Choi <41516670+doseee@users.noreply.github.com>
2024-03-01 21:40:22 +0900

    Title: 특정 구간의 원소 평균값, Time: 101ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240301/특정 구간의 원소 평균값/elemental-mean-value-for-a-particular-interval.java
@@ -0,0 +1,48 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+  static int n, ans;
+  static int[] a;
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    n = Integer.parseInt(br.readLine());
+    StringTokenizer st = new StringTokenizer(br.readLine());
+
+    a = new int[n];
+    for (int i = 0; i < n; i++)
+      a[i] = Integer.parseInt(st.nextToken());
+
+    ans = n;
+    for (int i = 2; i < n; i++)
+      calc(i);
+
+    System.out.print(ans);
+  }
+
+  public static void check(double x, int s, int e) {
+    for (int i = s; i < e; i++) {
+      if (x == a[i]) {
+        ans++;
+        return;
+      }
+    }
+  }
+
+  public static void calc(int d) {
+    int sum = 0;
+    for (int i = 0; i < d; i++)
+      sum += a[i];
+
+    check((double) sum / d, 0, d);
+
+    for (int i = d; i < n; i++) {
+      sum -= a[i - d];
+      sum += a[i];
+      check((double) sum / d, i - d, i);
+    }
+  }
+}
