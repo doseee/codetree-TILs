b41806562114f03213e5d1c72c32ee7dcac1550a
Eunnyeong Choi <41516670+doseee@users.noreply.github.com>
2024-03-01 22:19:42 +0900

    Title: 아름다운 수열 2, Time: 94ms, Memory: 9MB, Status: Passed - CodeTree

--- 240301/아름다운 수열 2/beautiful-sequence-2.java
+++ 240301/아름다운 수열 2/beautiful-sequence-2.java
@@ -4,30 +4,26 @@ import java.util.Map.Entry;
 
 public class Main {
 
-  static int n, m, ans;
-  static int[] a;
-  static HashMap<Integer, Integer> h;
-
-  public static void main(String[] args) throws IOException {
+ public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
     StringTokenizer st = new StringTokenizer(br.readLine());
-    n = Integer.parseInt(st.nextToken());
-    m = Integer.parseInt(st.nextToken());
+    int n = Integer.parseInt(st.nextToken());
+    int m = Integer.parseInt(st.nextToken());
 
-    a = new int[n];
+    int[] a = new int[n];
     st = new StringTokenizer(br.readLine());
     for (int i = 0; i < n; i++)
       a[i] = Integer.parseInt(st.nextToken());
 
-    h = new HashMap<>();
+    HashMap<Integer, Integer> h = new HashMap<>();
     st = new StringTokenizer(br.readLine());
     while (st.hasMoreTokens()) {
       int x = Integer.parseInt(st.nextToken());
       h.put(x, h.getOrDefault(x, 0) + 1);
     }
 
-    ans = 0;
+    int ans = 0;
     for (int i = 0; i < n - m + 1; i++) {
       HashMap<Integer, Integer> t = new HashMap<>();
       for (int j = i; j < i + m; j++) {
