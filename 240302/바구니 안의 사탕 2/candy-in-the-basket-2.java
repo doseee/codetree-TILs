8cb5db289a2b2273f3943f34698e0764cdfab28d
Eunnyeong Choi <41516670+doseee@users.noreply.github.com>
2024-03-02 11:45:59 +0900

    Title: 바구니 안의 사탕 2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240302/바구니 안의 사탕 2/candy-in-the-basket-2.java
@@ -0,0 +1,36 @@
+import java.util.*;
+import java.io.*;
+import java.util.Map.Entry;
+
+public class Main {
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    int n = Integer.parseInt(st.nextToken());
+    int k = Integer.parseInt(st.nextToken());
+
+    int[] a = new int[101];
+    for (int i = 0; i < n; i++) {
+      st = new StringTokenizer(br.readLine());
+      int x = Integer.parseInt(st.nextToken());
+      int y = Integer.parseInt(st.nextToken());
+      a[y] += x;
+    }
+
+    int sum = 0, ans = 0;
+    for (int i = 0; i <= k * 2; i++)
+      sum += a[i];
+
+    ans = Math.max(ans, sum);
+
+    for (int i = k * 2; i < 100; i++) {
+      sum -= a[i - (k * 2)];
+      sum += a[i + 1];
+      ans = Math.max(ans, sum);
+    }
+
+    System.out.print(ans);
+  }
+}
