64e6a060d692edc3ad89bb9dc8444f6327715e8c
Eunnyeong Choi <41516670+doseee@users.noreply.github.com>
2024-03-05 00:30:47 +0900

    Title: Date to Date, Time: 88ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240305/Date to Date/date-to-date.java
@@ -0,0 +1,31 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine());
+
+    int[] m = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    int a = Integer.parseInt(st.nextToken());
+    int b = Integer.parseInt(st.nextToken());
+    int c = Integer.parseInt(st.nextToken());
+    int d = Integer.parseInt(st.nextToken());
+
+    int ans = 1;
+    while(true) {
+      if (a != c) {
+        ans += m[a] - b;
+        b = 0;
+        a++;
+      }
+      else {
+        ans += d;
+        break;
+      }
+    }
+
+    System.out.print(ans);
+  }
+}
