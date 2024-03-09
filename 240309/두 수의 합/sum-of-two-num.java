8c6669533464300b0e8c7bb446129b0ea85c6aca
Eunnyeong Choi <41516670+doseee@users.noreply.github.com>
2024-03-09 11:08:07 +0900

    Title: 두 수의 합, Time: 411ms, Memory: 26MB, Status: Passed - CodeTree

--- 240309/두 수의 합/sum-of-two-num.java
+++ 240309/두 수의 합/sum-of-two-num.java
@@ -1,6 +1,5 @@
 import java.util.*;
 import java.io.*;
-import java.util.Map.Entry;
 
 public class Main {
 
@@ -11,31 +10,24 @@ public class Main {
     int n = Integer.parseInt(st.nextToken());
     int k = Integer.parseInt(st.nextToken());
 
-    HashMap<Long, Long> h = new HashMap<>();
+    long[] a = new long[n];
     st = new StringTokenizer(br.readLine());
     for (int i = 0; i < n; i++) {
-      Long x = Long.parseLong(st.nextToken());
-      h.put(x, h.getOrDefault(x, Long.valueOf(0)) + 1);
+      long x = Long.parseLong(st.nextToken());
+      a[i] = x;
     }
 
-    Long ans = Long.valueOf(0);
-    for (Entry<Long, Long> e : h.entrySet()) {
-      Long x = e.getKey();
-      Long y = k - x;
-      if (h.containsKey(y)) {
-        Long a = ((e.getValue() * (e.getValue() - 1)) / 2);
-        Long b = ((h.get(y) * (h.get(y) - 1)) / 2);
-        if (a == 0)
-          a = Long.valueOf(1);
-        if (b == 0)
-          b = Long.valueOf(1);
-        if (a == b && a == 1)
-          ans += a * b;
-        else
-          ans += a + b;
-      }
+    int ans = 0;
+    HashMap<Long, Long> h = new HashMap<>();
+    for (int i = 0; i < n; i++) {
+     long x = k - a[i];
+
+     if (h.containsKey(x))
+       ans += h.get(x);
+
+      h.put(a[i], h.getOrDefault(a[i], Long.valueOf(0)) + 1);
     }
 
-    System.out.print(ans / 2);
+    System.out.print(ans);
   }
 }
