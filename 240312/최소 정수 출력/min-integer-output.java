6140b03554aed834c73cc895ce33368ce3bc0e32
Eunnyeong Choi <41516670+doseee@users.noreply.github.com>
2024-03-12 23:48:30 +0900

    Title: 최소 정수 출력, Time: 472ms, Memory: 28MB, Status: Passed - CodeTree

---
+++ 240312/최소 정수 출력/min-integer-output.java
@@ -0,0 +1,29 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    int n = Integer.parseInt(br.readLine());
+
+    PriorityQueue<Integer> q = new PriorityQueue<>();
+    StringBuilder sb = new StringBuilder();
+    for (int i = 0; i < n; i++) {
+      int x = Integer.parseInt(br.readLine());
+      if (x != 0)
+        q.add(x);
+      else {
+        if (q.isEmpty())
+          sb.append(0 + "\n");
+        else
+          sb.append(q.poll() + "\n");
+      }
+    }
+
+    bw.write(sb.toString());
+    bw.close(); br.close();
+  }
+}
