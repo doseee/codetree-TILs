2a2f38ec6d0647950eff804c206e4069f7ca47cd
Eunnyeong Choi <41516670+doseee@users.noreply.github.com>
2024-03-18 21:39:10 +0900

    Title: 안전 지대, Time: 89ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240318/안전 지대/comfort-zone.java
@@ -0,0 +1,62 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+  static int[][] a;
+  static int[] dx = {1, 0, 0, -1}, dy = {0, 1, -1, 0};
+  static int n, m;
+  static boolean[][] visit;
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    n = Integer.parseInt(st.nextToken());
+    m = Integer.parseInt(st.nextToken());
+
+    a = new int[n][m];
+    for (int i = 0; i < n; i++) {
+      st = new StringTokenizer(br.readLine());
+      for (int j = 0; j < m; j++)
+        a[i][j] = Integer.parseInt(st.nextToken());
+    }
+
+    int ans = 0, k = 0;
+    for (int d = 1; d <= n * m; d++) {
+      int idx = 0;
+      visit = new boolean[n][m];
+      for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+          if (a[i][j] > d && !visit[i][j]) {
+            idx++;
+            dfs(i, j, d);
+          }
+        }
+      }
+      if (ans < idx) {
+        ans = idx;
+        k = d;
+      }
+
+    }
+
+    System.out.println(ans + " " + k);
+
+  }
+
+  public static void dfs(int x, int y, int d) {
+    visit[x][y] = true;
+
+    for (int k = 0; k < 4; k++) {
+      int nx = x + dx[k], ny = y + dy[k];
+      if (range(nx, ny) && a[nx][ny] > d && !visit[nx][ny]) {
+        visit[nx][ny] = true;
+        dfs(nx, ny, d);
+      }
+    }
+  }
+
+  public static boolean range(int nx, int ny) {
+    return nx >= 0 && nx < n && ny >= 0 && ny < m;
+  }
+}
