f365004c6f9b814fd0a6e1f18515e64c59786aca
Eunnyeong Choi <41516670+doseee@users.noreply.github.com>
2024-03-19 12:23:16 +0900

    Title: 뿌요뿌요, Time: 94ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240319/뿌요뿌요/puyo-puyo.java
@@ -0,0 +1,55 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+  static int[][] a;
+  static int[] dx = {1, 0, 0, -1}, dy = {0, 1, -1, 0};
+  static int n, cnt;
+  static boolean[][] visit;
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    n = Integer.parseInt(br.readLine());
+
+    a = new int[n][n];
+    for (int i = 0; i < n; i++) {
+      StringTokenizer st = new StringTokenizer(br.readLine());
+      for (int j = 0; j < n; j++)
+        a[i][j] = Integer.parseInt(st.nextToken());
+    }
+
+    int idx = 0, max = 0;
+    visit = new boolean[n][n];
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < n; j++) {
+        if (!visit[i][j]) {
+          cnt = 1;
+          dfs(i, j, a[i][j]);
+          if (cnt >= 4)
+            idx = a[i][j];
+          max = Math.max(max, cnt);
+        }
+      }
+    }
+
+    System.out.print(idx + " " + max);
+  }
+
+  public static void dfs(int x, int y, int d) {
+    visit[x][y] = true;
+
+    for (int k = 0; k < 4; k++) {
+      int nx = x + dx[k], ny = y + dy[k];
+      if (range(nx, ny) && a[nx][ny] == d && !visit[nx][ny]) {
+        cnt++;
+        visit[nx][ny] = true;
+        dfs(nx, ny, d);
+      }
+    }
+  }
+
+  public static boolean range(int nx, int ny) {
+    return nx >= 0 && nx < n && ny >= 0 && ny < n;
+  }
+}
