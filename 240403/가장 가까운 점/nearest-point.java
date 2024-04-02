e34987153379b73056fd12eba4a5171556731866
Eunnyeong Choi <41516670+doseee@users.noreply.github.com>
2024-04-03 00:03:37 +0900

    Title: 가장 가까운 점, Time: 370ms, Memory: 21MB, Status: Wrong Answer - CodeTree

---
+++ 240403/가장 가까운 점/nearest-point.java
@@ -0,0 +1,48 @@
+import java.awt.Point;
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    int n = Integer.parseInt(st.nextToken());
+    int m = Integer.parseInt(st.nextToken());
+
+    PriorityQueue<Point> q = new PriorityQueue<>(new Comparator<Point>() {
+      @Override
+      public int compare(Point o1, Point o2) {
+        int x = Math.abs(o1.x) + Math.abs(o1.y);
+        int y = Math.abs(o2.x) + Math.abs(o2.y);
+        if (x == y) {
+          if (o1.x == o2.x)
+            return o1.y - o2.y;
+          return o1.x - o2.x;
+        }
+        else {
+          if (x > y)
+            return 1;
+          else
+            return 0;
+        }
+      }
+    });
+
+    for (int i = 0; i < n; i++) {
+      st = new StringTokenizer(br.readLine());
+      int x = Integer.parseInt(st.nextToken());
+      int y = Integer.parseInt(st.nextToken());
+      q.add(new Point(x, y));
+    }
+
+    for (int i = 0; i < m; i++) {
+      Point p = q.poll();
+      q.add(new Point(p.x + 2, p.y + 2));
+    }
+
+    Point ans = q.poll();
+    System.out.print(ans.x + " " + ans.y);
+  }
+}
