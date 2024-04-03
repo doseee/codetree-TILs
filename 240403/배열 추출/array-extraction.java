0672fe43c70be5d8e8b1c8c5b2c69eb0778e4990
Eunnyeong Choi <41516670+doseee@users.noreply.github.com>
2024-04-03 23:23:44 +0900

    Title: 배열 추출, Time: 883ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240403/배열 추출/array-extraction.java
@@ -0,0 +1,30 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    int n = Integer.parseInt(br.readLine());
+
+    PriorityQueue<Integer> q = new PriorityQueue<>(new Comparator<Integer>() {
+      @Override
+      public int compare(Integer o1, Integer o2) {
+        return o2 - o1;
+      }
+    });
+
+    for (int i = 0; i < n; i++) {
+     int x = Integer.parseInt(br.readLine());
+     if (x != 0)
+       q.add(x);
+     else {
+       if (q.isEmpty())
+         System.out.println("0");
+       else
+         System.out.println(q.poll());
+     }
+    }
+  }
+}
