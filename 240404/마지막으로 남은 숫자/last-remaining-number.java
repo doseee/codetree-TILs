59145b07b4cf751fd094eca1ce23c620fbce62d3
Eunnyeong Choi <41516670+doseee@users.noreply.github.com>
2024-04-04 23:52:36 +0900

    Title: 마지막으로 남은 숫자, Time: 432ms, Memory: 22MB, Status: Passed - CodeTree

--- 240404/마지막으로 남은 숫자/last-remaining-number.java
+++ 240404/마지막으로 남은 숫자/last-remaining-number.java
@@ -20,19 +20,26 @@ public class Main {
     for (int i = 0; i < n; i++)
       q.add(Integer.parseInt(st.nextToken()));
 
-    int ans = 0;
+    int ans = -1;
     while (!q.isEmpty()) {
       int x = q.poll();
       int y = -1;
+
       if (!q.isEmpty())
         y = q.poll();
-      if (y == -1)
-        ans = y;
       else {
-        q.add(Math.abs(x - y));
-      }
-      if (q.isEmpty())
         ans = x;
+        break;
+      }
+
+      if (y != -1) {
+        int t = Math.abs(x - y);
+        if (t != 0)
+          q.add(t);
+      }
+
+      if (q.size() == 1)
+        ans = q.poll();
     }
 
     System.out.print(ans);
