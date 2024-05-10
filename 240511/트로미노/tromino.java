de3d6627fb47a4ed13968c4e3e27ebc73d507f00
Eunnyeong Choi <41516670+doseee@users.noreply.github.com>
2024-05-11 02:21:32 +0900

    Title: 트로미노, Time: 180ms, Memory: 12MB, Status: Passed - CodeTree

--- 240511/트로미노/tromino.java
+++ 240511/트로미노/tromino.java
@@ -24,7 +24,7 @@ public class Main {
 
     ans = 0;
     for (int i = 0; i < n; i++) {
-      for (int j = 0; j < n; j++) {
+      for (int j = 0; j < m; j++) {
         check1(i, j);
         check2(i, j);
       }
