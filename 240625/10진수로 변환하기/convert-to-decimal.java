1e0f6e01d0b9fde908d2cd0e8bf0161f10e8a1b3
Eunnyeong Choi <41516670+doseee@users.noreply.github.com>
2024-06-25 02:00:32 +0900

    Title: 10진수로 변환하기, Time: 132ms, Memory: 8MB, Status: Passed - CodeTree

--- 240625/10진수로 변환하기/convert-to-decimal.java
+++ 240625/10진수로 변환하기/convert-to-decimal.java
@@ -3,8 +3,7 @@ import java.io.*;
 public class Main {
     public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
-        String s = br.readLine();
-        System.out.println(Integer.parseInt(s, 2));
+
+        System.out.println(Integer.parseInt(br.readLine(), 2));
     }
 }
