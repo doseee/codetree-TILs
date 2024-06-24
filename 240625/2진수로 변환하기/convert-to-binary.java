93af8549beee9eb9e7686a3ea70f1621d77df4c9
Eunnyeong Choi <41516670+doseee@users.noreply.github.com>
2024-06-25 01:28:32 +0900

    Title: 2진수로 변환하기, Time: 93ms, Memory: 8MB, Status: Passed - CodeTree

--- 240625/2진수로 변환하기/convert-to-binary.java
+++ 240625/2진수로 변환하기/convert-to-binary.java
@@ -6,13 +6,12 @@ public class Main {
 
         int n = Integer.parseInt(br.readLine());
 
-        String s = "";
+        StringBuffer s = new StringBuffer();
         while(true) {
-            s += n % 2;
+            s.append(n % 2);
             n /= 2;
             if (n == 0) break;
         }
-        for (int i = s.length() - 1; i>=0; i--)
-            System.out.print(s.charAt(i));
+       System.out.println(s.reverse());
     }
 }
