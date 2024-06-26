8c1b5444bf04137e45b4f2fc336bfb991c22cfda
Eunnyeong Choi <41516670+doseee@users.noreply.github.com>
2024-06-27 02:23:51 +0900

    Title: 연속된 숫자 만들기 2, Time: 93ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240627/연속된 숫자 만들기 2/create-consecutive-numbers-2.java
@@ -0,0 +1,36 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    
+    static int ans;
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+
+        ans = Integer.MAX_VALUE;
+        dfs(a, b, c, 0);
+
+        System.out.print(ans);
+    }
+
+    public static void dfs(int a, int b, int c, int cnt) {
+        if (a == b || b == c || a == c) return;
+        if (c - b == 1 && b - a == 1) {
+            ans = Math.min(cnt, ans);
+            return;
+        }
+
+        //a를 옮기는 경우
+        dfs(b, b + 1, c, cnt + 1);
+        dfs(b, c - 1, c, cnt + 1);
+
+        //c를 옮기는 경우
+        dfs(a, a + 1, b, cnt + 1);
+        dfs(a, b - 1, b, cnt + 1);
+    }
+}
