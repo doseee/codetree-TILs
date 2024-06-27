88df18a4383e649f9d2807590c99bf4054c9595d
Eunnyeong Choi <41516670+doseee@users.noreply.github.com>
2024-06-28 04:58:36 +0900

    Title: 독서실의 거리두기 3, Time: 82ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240628/독서실의 거리두기 3/study-cafe-keeping-distance-3.java
@@ -0,0 +1,32 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        String s = br.readLine();
+        int ans = 0, t = 0, idx = 0;
+        HashSet<Integer> h = new HashSet<>();
+        for (int i = 1; i < s.length() - 1; i++) {
+            if (s.charAt(i) == '1') {
+                if (ans < i - t) {
+                    ans = i - t;
+                    idx = i;
+                }
+                t = i;
+            }
+
+            if (s.charAt(i - 1) == s.charAt(i + 1) && s.charAt(i) == '0' && s.charAt(i + 1) == '0') {
+                h.add(i);
+            }
+        }
+
+        if (h.size() != 1) {
+            ans /= 2;
+        }
+
+        System.out.print(ans);
+    }
+}
