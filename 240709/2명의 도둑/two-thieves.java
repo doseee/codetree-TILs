3db07c35f314371655ff9adc721c77cecfa44401
Eunnyeong Choi <41516670+doseee@users.noreply.github.com>
2024-07-09 08:21:26 +0900

    Title: 2명의 도둑, Time: 101ms, Memory: 8MB, Status: Wrong Answer - CodeTree

--- 240709/2명의 도둑/two-thieves.java
+++ 240709/2명의 도둑/two-thieves.java
@@ -3,13 +3,19 @@ import java.io.*;
 import java.awt.Point;
 
 public class Main {
+    
+    static int m, c, num;
+    static int[] t;
+    static boolean[] select;
+    static List<Point> ans;
+
     public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
-        int c = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        c = Integer.parseInt(st.nextToken());
 
         int[][] a = new int[n][n];
         for (int i = 0; i < n; i++) {
@@ -19,25 +25,17 @@ public class Main {
         }
 
 
-        List<Point> ans = new ArrayList<>();
-        int num = 0;
+        ans = new ArrayList<>();
+        num = 0;
         for (int i = 0; i < n; i++) {
-            ex:for (int j = 0; j <= n - m; j++) {
-                Integer[] t = new Integer[m];
+            for (int j = 0; j <= n - m; j++) {
+                t = new int[m];
                 int idx = 0;
                 for (int k = j; k < j + m; k++)
                     t[idx++] = a[i][k];
 
-                Arrays.sort(t, Collections.reverseOrder());
-
-                int chk = 0, sum = 0;
-                for (int k = 0; k < m; k++) {
-                    if (chk + t[k] <= c) {
-                        chk += t[k];
-                        sum += t[k] * t[k];
-                    }
-                }
-                ans.add(new Point(sum, num++));
+                select = new boolean[m];
+                subset(0);
             }
             num+=n;
         }
@@ -54,11 +52,32 @@ public class Main {
         int rslt = ans.get(0).x;
         int t = ans.get(0).y;
         for (int i = 1; i < ans.size(); i++) {
-            if (Math.abs(t - ans.get(i).y) >= m) {
+            if (Math.abs(t - ans.get(i).y) >= Math.pow(m, 2)) {
                 rslt += ans.get(i).x;
                 break;
             }
         }
         System.out.print(rslt);
+    }
+
+    public static void subset(int cnt) {
+        if (cnt == m) {
+            int chk = 0, sum = 0;
+            for (int i = 0; i < m; i++) {
+                if (select[i]) {
+                    if (chk + t[i] <= c) {
+                        chk += t[i];
+                        sum += t[i] * t[i];
+                    }
+                }
+            }
+            ans.add(new Point(sum, num++));
+            return;
+        }
+
+        select[cnt] = true;
+        subset(cnt + 1);
+        select[cnt] = false;
+        subset(cnt + 1);
     }
 }
