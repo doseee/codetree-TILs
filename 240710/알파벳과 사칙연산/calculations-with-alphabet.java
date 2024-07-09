bd0a677fc783c1656e8a7c56e9283de0ca558ab0
Eunnyeong Choi <41516670+doseee@users.noreply.github.com>
2024-07-10 02:38:55 +0900

    Title: 알파벳과 사칙연산, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240710/알파벳과 사칙연산/calculations-with-alphabet.java
@@ -0,0 +1,65 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+  static HashSet<Character> list;
+  static int[] select, a = {1, 2, 3, 4};
+  static int ans;
+  static String s;
+  
+  public static void main(String[] args) throws Exception{
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    s = br.readLine();
+
+    list = new HashSet<>();
+    for (int i = 0; i < s.length(); i++) {
+      char c = s.charAt(i);
+      if (Character.isAlphabetic(c))
+        list.add(c);
+    }
+
+    ans = 0;
+    select = new int[list.size()];
+    perm(0);
+
+    System.out.print(ans);
+  }
+
+  public static int cal(int x, int y, char o) {
+    if (o == '-')
+      return x - y;
+    if (o == '+')
+      return x + y;
+    if (o == '*')
+      return x * y;
+    return x / y;
+  }
+
+  public static void calc() {
+    HashMap<Character, Integer> h = new HashMap<>();
+    int idx = 0;
+    for (Character c : list)
+      h.put(c, select[idx++]);
+
+    int sum = cal(h.get(s.charAt(0)), h.get(s.charAt(2)), s.charAt(1));
+    for (int i = 3; i < s.length() - 1; i+=2)
+      sum = cal(sum, h.get(s.charAt(i + 1)), s.charAt(i));
+
+    ans = Math.max(ans, sum);
+  }
+
+  public static void perm(int cnt) {
+    if (cnt == list.size()) {
+      calc();
+      return;
+    }
+
+    for (int i = 0; i < 4; i++) {
+      select[cnt] = a[i];
+      perm(cnt + 1);
+    }
+  }
+
+
+}
