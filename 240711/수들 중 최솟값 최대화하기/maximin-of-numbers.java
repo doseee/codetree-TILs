dc31063643653865c2a33f1a14ee5f63f9b7b860
Eunnyeong Choi <41516670+doseee@users.noreply.github.com>
2024-07-11 03:39:54 +0900

    Title: 수들 중 최솟값 최대화하기, Time: 128ms, Memory: 9MB, Status: Passed - CodeTree

--- 240711/수들 중 최솟값 최대화하기/maximin-of-numbers.java
+++ 240711/수들 중 최솟값 최대화하기/maximin-of-numbers.java
@@ -1,77 +1,52 @@
 import java.util.*;
 import java.io.*;
-import java.awt.Point;
 
 public class Main {
 
   static int n, ans;
-  static int[] select;
-  static boolean[] visit;
   static int[][] map;
-  static Point[] list;
 
   public static void main(String[] args) throws Exception{
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
     n = Integer.parseInt(br.readLine());
-
-    int idx = 0;
-    list = new Point[n * n];
     map = new int[n][n];
     for (int i = 0; i < n; i++) {
       StringTokenizer st = new StringTokenizer(br.readLine());
-      for (int j = 0; j < n; j++) {
-        list[idx++] = new Point(i, j);
+      for (int j = 0; j < n; j++)
         map[i][j] = Integer.parseInt(st.nextToken());
-      }
     }
 
     ans = 0;
-    select = new int[n];
-    visit = new boolean[n * n];
-    perm(0, 0);
+    for (int i = 0; i < n; i++) {
+      boolean[] visit = new boolean[n];
+      visit[i] = true;
+      int[] select = new int[n];
+      select[0] = i;
+      dfs(select, visit, 1);
+    }
 
     System.out.print(ans);
   }
 
-  public static boolean check() {
-    HashSet<Integer> x = new HashSet<>();
-    HashSet<Integer> y = new HashSet<>();
-
-    for (int i = 0; i < n; i++) {
-      Point p = list[select[i]];
-      x.add(p.x);
-      y.add(p.y);
+  public static void dfs(int[] select, boolean[] visit, int d) {
+    if (d == n) {
+      int min = Integer.MAX_VALUE;
+      
+      for (int i = 0; i < n; i++)
+        min = Math.min(min, map[i][select[i]]);
+        
+      ans = Math.max(ans, min);
+      return;
     }
 
-    if (x.size() == n && y.size() == n)
-      return true;
-
-    return false;
-  }
-
-  public static void calc() {
-    int min = Integer.MAX_VALUE;
-
     for (int i = 0; i < n; i++) {
-      Point p = list[select[i]];
-      min = Math.min(min, map[p.x][p.y]);
+      if (!visit[i]) {
+        select[d] = i;
+        visit[i] = true;
+        dfs(select, visit, d + 1);
+        visit[i] = false;
+      }
     }
-
-    ans = Math.max(ans, min);
-  }
-
-  public static void perm(int cnt, int bitMask) {
-        if (cnt == n) {
-            if (check()) calc();
-            return;
-        }
-
-        for (int i = 0; i < n * n; i++) {
-            if ((bitMask & (1 << i)) == 0) {
-                select[cnt] = i;
-                perm(cnt + 1, bitMask | (1 << i));
-            }
-        }
   }
 }
