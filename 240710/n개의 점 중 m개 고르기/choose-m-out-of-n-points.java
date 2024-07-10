9d09d1daa124b63d4ca440c2a4da7fc8c1aee0f1
Eunnyeong Choi <41516670+doseee@users.noreply.github.com>
2024-07-10 21:36:59 +0900

    Title: n개의 점 중 m개 고르기, Time: 77ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240710/n개의 점 중 m개 고르기/choose-m-out-of-n-points.java
@@ -0,0 +1,60 @@
+import java.util.*;
+import java.io.*;
+import java.awt.Point;
+
+public class Main {
+
+  static int n, m, ans;
+  static int[] select, two;
+  static Point[] list;
+
+  public static void main(String[] args) throws Exception{
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine());
+
+    n = Integer.parseInt(st.nextToken());
+    m = Integer.parseInt(st.nextToken());
+
+    list = new Point[n];
+    for (int i = 0; i < n; i++) {
+      st = new StringTokenizer(br.readLine());
+      int x = Integer.parseInt(st.nextToken());
+      int y = Integer.parseInt(st.nextToken());
+      list[i] = new Point(x, y);
+    }
+
+    ans = Integer.MAX_VALUE;
+    select = new int[m];
+    two = new int[2];
+    comb(0, 0);
+
+    System.out.print(ans);
+  }
+
+  public static void choice(int cnt, int idx) {
+    if (cnt == 2) {
+      Point p1 = list[two[0]], p2 = list[two[1]];
+      int x = p1.x - p2.x, y = p1.y - p2.y;
+      x *= x; y *= y;
+      ans = Math.min(ans, x + y);
+      return;
+    }
+
+    for (int i = idx; i < m; i++) {
+      two[cnt] = select[i];
+      choice(cnt + 1, i + 1);
+    }
+  }
+
+  public static void comb(int cnt, int idx) {
+    if (cnt == m) {
+      choice(0, 0);
+      return;
+    }
+
+    for (int i = idx; i < n; i++) {
+      select[cnt] = i;
+      comb(cnt + 1, i + 1);
+    }
+  }
+}
